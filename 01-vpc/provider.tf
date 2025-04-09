1b66aacee199be5a67303f836ead3d7f3ca2b249
nnreddybalreddy <narendra.mahn@gmail.com>
2025-04-08 20:43:35 -0400

    ALl

--- 01-vpc/provider.tf
+++ 01-vpc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "78s-remote-state-1"
-    key            = "vpc26-terraform-expense"
+    key            = "vpc27-terraform-expense"
     region         = "us-east-1"
     dynamodb_table = "daws78s-locking"
   }
